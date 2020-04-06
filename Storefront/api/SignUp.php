f31f88bbfcd8142b6db13c6c0edf9e2668d13dee
Ahmed <ahmedmansour@knights.ucf.edu>
2020-04-06 15:38:45 -0400

    updated files

--- Storefront/api/SignUp.php
+++ Storefront/api/SignUp.php
@@ -3,14 +3,21 @@
     // Extract users information from the json
     $inData = getRequestInfo();
     
-    $userName = $inData["userName"];
+    $userID = $inData["userID"];
+	$userName = $inData["userName"];
+	$firstName = $inData["firstName"];
+	$lastName = $inData["lastName"];
+	$email = $inData["email"];
+	$phone = $inData["phone"];
+	$address = $inData["address"];
     $passWord = md5($inData["passWord"]);
-    $firstName = $inData["firstName"];
-    $lastName = $inData["lastName"];
-    $email = $inData["email"];
+	$adminFlag = $inData["adminFlag"];
+   
+
+
     
     // Connect to the database
-    $conn = new mysqli("127.0.0.1", "root", "", "ManagerDB");
+    $conn = new mysqli("107.180.47.58", "ahmed", "2KDKRpen3%ty", "amazoffdb");
     
     if ( $conn->connect_error )
     {
@@ -19,7 +26,7 @@
     else
     {
         // Before inserting the user, check to make sure that username is not already taken
-        $checkStatus = "SELECT userName FROM Users WHERE userName='".$userName."'";
+        $checkStatus = "SELECT Email FROM Users WHERE email='".$email."'";
         $alreadyTaken = $conn->query($checkStatus);
         
         // If the query returned any rows, that indicates there is alredy a user with that username
@@ -30,7 +37,7 @@
         else
         {
             // If we get here the username is available. This creates an sql statement to insert the user
-            $sql = "INSERT INTO Users (userName, passWord, firstName, lastName, email) VALUES ('$userName', '$passWord', '$firstName', '$lastName', '$email')";
+            $sql = "INSERT INTO Users (UserId, UserName, FirstName, LastName, Email, Phone, Address, Password, AdminFlag) VALUES ('$userID', '$userName', '$firstName', '$lastName', '$email', '$phone', '$address', '$passWord', '$adminFlag')";
             
             // Insert the user, if its not possible, return an error
             if ( $result = $conn->query($sql) != TRUE)
@@ -38,7 +45,7 @@
                 returnWithError ($conn->error);
             }
             
-            returnWithInfo($firstName, $lastName);
+            returnWithInfo();
         }
         
         // Lastly close the connection to the database
@@ -59,9 +66,9 @@
     }
     
     // Creates a json package with information provided by the user
-    function returnWithInfo($firstName, $lastName)
+    function returnWithInfo()
     {
-        $retValue = '{"Mess":"' . "Success!" . '","firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+        $retValue = '{"Mess":' . "Success!" . '}';
         sendResultInfoAsJson($retValue);
     }
     
--- Storefront/js/code.js
+++ Storefront/js/code.js
@@ -1,29 +1,199 @@
 
+var urlBase = 'http://amazoff.fun';
+var extension = 'php';
+console.log(urlBase);
+var userId = 0;
+var firstName = "";
+var lastName = "";
 
-const {MongoClient} = require('mongodb');
 
-async function main() {
-	// The connection string we use to get into the database
-	const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
+// grabs values entered into text fields on sign up page
+function doSignUp()
+{
+	var  email = document.getElementById("email").innerHTMl;
+	var  userName = document.getElementById("userName").innerHTMl;
+	var  passWord = document.getElementById("passWord").innerHTMl;
+	var  firstName = document.getElementById("firstName").innerHTMl;
+	var  lastName = document.getElementById("lastName").innerHTMl;
+	console.log(email + " " + userName + " " + passWord + " " + firstName + " " + lastName);
+}
+
+// grabs values entered into text fields on login in page and sends to server via JSON package
+function doLogin()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
 	
-	// Create mongo object
-	const client = new MongoClient(uri);
+	var login = document.getElementById("loginName").value;
+	var password = document.getElementById("loginPassword").value;
 	
-	try {
-		// Actual connection of database
-		await client.connect();
+	document.getElementById("loginResult").innerHTML = "";
+
+	var jsonPayload = '{"userName" : "' + login + '", "passWord" : "' + password + '"}';
+	var url = urlBase + '/api/login.' + extension;
+	// debugging
+	console.log(url);
+	console.log(jsonPayload);
+	// end
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, false);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
 		
-		// test function that lists all the databases we have
-		await listDatabases(client);
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.UID;
+		console.log(userId);
+		if( userId < 1 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+		}
+		
+		firstName = jsonObject.firstName;
+		lastName = jsonObject.lastName;
+
+		saveCookie();
+
+		console.log("search.html?firstName=" + firstName +"&lastName=" + lastName);
+		window.location.href= "search.html?firstName=" +firstName + "&lastName=" + lastName;
 	}
-	catch (e) {
-		console.error(e);
+	catch(err)
+	{
+		document.getElementById("loginResult").innerHTML = err.message;
 	}
-	finally {
-		// Close the connection
-		await client.close();
+
+}
+
+function saveCookie()
+{
+	var minutes = 20;
+	var date = new Date();
+	date.setTime(date.getTime()+(minutes*60*1000));	
+	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
+}
+
+function readCookie()
+{
+	userId = -1;
+	var data = document.cookie;
+	var splits = data.split(",");
+	for(var i = 0; i < splits.length; i++) 
+	{
+		var thisOne = splits[i].trim();
+		var tokens = thisOne.split("=");
+		if( tokens[0] == "firstName" )
+		{
+			firstName = tokens[1];
+		}
+		else if( tokens[0] == "lastName" )
+		{
+			lastName = tokens[1];
+		}
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
+		}
+	}
+	
+	if( userId < 0 )
+	{
+		window.location.href = "index.html";
+	}
+	else
+	{
+		document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	}
+}
+
+function doLogout()
+{
+	userId = 0;
+	firstName = "";
+	lastName = "";
+	document.cookie = "firstName= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+	window.location.href = "index.html";
+}
+
+function addContact()
+{
+	var firstName = document.getElementById("firstName").value;
+	var lastName = document.getElementById("lastName").value;
+	var email = document.getElementById("email").value;
+	var phoneNumber = document.getElementById("phoneNumber").value;
+	var notes = document.getElementById("notes").value;
+
+	document.getElementById("contactAddResult").innerHTML = "";
+	
+	var jsonPayload = '{"UID" : "' + userID + '", "firstName" : ' + firstName +
+					  '", "lastName" : ' + lastName + '", "email" : ' + email +
+					  '", "phoneNumber" : ' + phoneNumber + '", "notes" : ' + notes +'}';
+
+	var url = urlBase + '/AddContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactAddResult").innerHTML = "Contact has been added";
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactAddResult").innerHTML = err.message;
 	}
 	
 }
 
-main().catch(console.error);
+function doSearch()
+{
+	var srch = document.getElementById("searchBox").value;
+	document.getElementById("colorSearchResult").innerHTML = "";
+	
+	var colorList = "";
+	
+	var jsonPayload = '{"UID" : "' + userID + '","search" : ' + srch + '}';
+	var url = urlBase + '/SearchContact.' + extension;
+	
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.onreadystatechange = function() 
+		{
+			if (this.readyState == 4 && this.status == 200) 
+			{
+				document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
+				var jsonObject = JSON.parse(xhr.responseText);
+				
+				for(var i = 0; i < jsonObject.results.length; i++)
+				{
+					contactList += jsonObject.results[i];
+					if( i < jsonObject.results.length - 1 )
+					{
+						contactList += "<br />\r\n";
+					}
+				}
+				
+				document.getElementsByTagName("p")[0].innerHTML = contactList;
+			}
+		};
+		xhr.send(jsonPayload);
+	}
+	catch(err)
+	{
+		document.getElementById("contactSearchResult").innerHTML = err.message;
+	}
+	
+}
